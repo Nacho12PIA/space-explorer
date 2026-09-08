a7fb0d32a50b47b043c51c0ce63646d7086458c1
Nacho12PIA <narodgui@gmail.com>
2026-09-08 12:28:26 +0200

    Update SolarSystem.js

--- components/SolarSystem.js
+++ components/SolarSystem.js
@@ -1399,7 +1399,7 @@ function JupiterAtmosphereMarkers({
     <JupiterHotspot
       size={size}
       latitude={-22}
-      longitude={0}
+      longitude={25}
       label="GRAN MANCHA ROJA"
       occluder={
         occluder

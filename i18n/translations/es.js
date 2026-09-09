16b4386567505fc2dce968ff469f6d7e3bec01e0
Nacho12PIA <narodgui@gmail.com>
2026-09-09 12:59:28 +0200

    Añade diccionario base en español para i18n

---
+++ i18n/translations/es.js
@@ -0,0 +1,19 @@
+const es = {
+  common: {
+    language: {
+      spanish: "Español",
+      english: "English",
+      switchToSpanish: "Cambiar a español",
+      switchToEnglish: "Cambiar a inglés",
+    },
+  },
+  home: {},
+  explore: {},
+  solarSystem: {},
+  planets: {},
+  missions: {},
+  laboratory: {},
+  nova: {},
+};
+
+export default es;

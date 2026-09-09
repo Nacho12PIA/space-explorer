ae3433e7c07b1419d3f29f9392ac6d1896dbd4c4
Nacho12PIA <narodgui@gmail.com>
2026-09-09 12:59:33 +0200

    Añade diccionario base en inglés para i18n

---
+++ i18n/translations/en.js
@@ -0,0 +1,19 @@
+const en = {
+  common: {
+    language: {
+      spanish: "Español",
+      english: "English",
+      switchToSpanish: "Switch to Spanish",
+      switchToEnglish: "Switch to English",
+    },
+  },
+  home: {},
+  explore: {},
+  solarSystem: {},
+  planets: {},
+  missions: {},
+  laboratory: {},
+  nova: {},
+};
+
+export default en;

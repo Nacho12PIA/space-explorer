dea9eee2e37406d44125a456d650fc5be4598ae2
Nacho12PIA <narodgui@gmail.com>
2026-09-10 15:34:52 +0200

    Wrap app with progress provider

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { LanguageProvider } from "../i18n/LanguageContext";
+import { ProgressProvider } from "../i18n/ProgressContext";
 import SiteHeader from "../components/SiteHeader";
 import SiteFooter from "../components/SiteFooter";
 
@@ -13,17 +14,19 @@ export default function RootLayout({ children }) {
     <html lang="es">
       <body>
         <LanguageProvider>
-          <div
-            style={{
-              minHeight: "100vh",
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <SiteHeader />
-            <div style={{ flex: 1, minWidth: 0 }}>{children}</div>
-            <SiteFooter />
-          </div>
+          <ProgressProvider>
+            <div
+              style={{
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <SiteHeader />
+              <div style={{ flex: 1, minWidth: 0 }}>{children}</div>
+              <SiteFooter />
+            </div>
+          </ProgressProvider>
         </LanguageProvider>
       </body>
     </html>
